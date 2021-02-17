dd2881550b3e10bb2b4bdb2640fff90e6d886bc2
George4888 <DanGeorge4888@gmail.com>
2021-02-17 17:52:26 +0200

    clean up and setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="question">
+        <h1>Question location</h1>
+      </div>
+      <div className="answer">
+        <p>answer 1</p>
+        <p>answer 2</p>
+        <p>answer 3</p>
+        <p>answer 4</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
