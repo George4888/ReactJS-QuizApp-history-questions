a15745f7aabc38f2d7d3364c3e66bd01f60186da
George4888 <dangeorge4888@gmail.com>
2021-02-18 12:46:43 +0200

    fetch API questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import './App.css';
+import GetQuestions from './GetQuestions';
 
 function App() {
   return (
     <div className="App">
-      <div className="question">
-        <h1>Question location</h1>
-      </div>
-      <div className="answer">
-        <p>answer 1</p>
-        <p>answer 2</p>
-        <p>answer 3</p>
-        <p>answer 4</p>
-      </div>
+      <GetQuestions />
     </div>
   );
 }
---
+++ src/GetQuestions.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+
+const GetQuestions = () => {
+    const API = 'https://opentdb.com/api.php?amount=25&category=23&type=multiple';
+
+    useEffect(() => {
+        fetchQuestions();
+    }, [])
+
+    const [questions, setQuestions] = useState([]);
+
+    const fetchQuestions = async () => {
+        const data = await fetch(API);
+        const questions = await data.json();
+        
+        console.log(questions.results);
+        setQuestions(questions.results);
+    }
+
+    return (
+        <div>
+            {questions.map(question => (
+                <>
+                <div className="question">
+                <h1 key={question.correct_answer}>{question.question}</h1>
+                </div>
+                <div className="answer">
+                    <p>{question.incorrect_answers}</p>
+                    <p>{question.correct_answer}</p>
+                    <p>{question.incorrect_answers}</p>
+                    <p>{question.incorrect_answers}</p>
+                </div>
+                </>
+            ))}
+        </div>
+    )
+}
+
+export default GetQuestions
