5a2c4117b5dc30fe13a279108bb72838dd5df2ec
George4888 <dangeorge4888@gmail.com>
2021-02-22 12:03:43 +0200

    score added to quiz

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,40 @@ import "./App.css";
 function App() {
   const API = "https://opentdb.com/api.php?amount=25&category=23&type=multiple";
   const [questions, setQuestions] = useState([]);
-  const [currentQuestion, setCurrentQuestion] = useState(undefined);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [questionsEnded, setQuestionsEnded] = useState(false);
 
   useEffect(() => {
     fetch(API).then((res) =>
       res.json().then((data) => {
         console.log(data.results);
         setQuestions(data.results);
-        setCurrentQuestion(data.results[0]);
       })
     );
   }, []);
 
   const handleAnswer = (answer) => {
+    const newIndex = currentIndex + 1;
+    setCurrentIndex(newIndex);
     // check for answer
+    if (answer === questions[currentIndex].correct_answer) {
+      // Increase score
+      setScore(score + 1);
+    }
+    if (newIndex >= questions.length) {
+      setQuestionsEnded(true);
+    }
     // show another answer
   };
 
-  return questions.length > 0 ? (
+  return questionsEnded ? (
+    <h1>
+      Your score was {score} out of {questions.length}
+    </h1>
+  ) : questions.length > 0 ? (
     <div className="App">
-      {currentQuestion && (
-        <Questions data={currentQuestion} handleAnswer={handleAnswer} />
-      )}
+      <Questions data={questions[currentIndex]} handleAnswer={handleAnswer} />
     </div>
   ) : (
     <h1>Loading...</h1>
